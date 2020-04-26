629db594cdf034dc354d8b9773ff238ddeeff852
Jan Socha <s0j0hn@protonmail.com>
2020-04-26 15:15:23 +0200

    Separete policy

--- config/config.go
+++ config/config.go
@@ -2,14 +2,7 @@ package config
 
 import (
 	"fmt"
-	"github.com/casbin/casbin/v2"
-	defaultrolemanager "github.com/casbin/casbin/v2/rbac/default-role-manager"
-	"github.com/casbin/casbin/v2/util"
-	gormadapter "github.com/casbin/gorm-adapter/v2"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
-	"log"
 )
 
 func getViper() *viper.Viper {
@@ -49,87 +42,3 @@ func GetDataBaseAccess() string {
 	return connection
 }
 
-func InitPolicy(gormClient *gorm.DB) (*casbin.Enforcer, error) {
-	// Initialize a Gorm adapter and use it in a Casbin enforcer:
-	// The adapter will use the MySQL database named "casbin".
-	// If it doesn't exist, the adapter will create it automatically.
-	casbinGormAdapter, err := gormadapter.NewAdapterByDB(gormClient) // Your driver and data source.
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	// We need a new roleManager to force the route params verification ex: (/tenants/:id).
-	var roleManager = defaultrolemanager.NewRoleManager(2)
-	roleManager.(*defaultrolemanager.RoleManager).AddMatchingFunc("KeyMatch2", util.KeyMatch2)
-
-	// Create Policy enforcer with our customized model.
-	policyEnforcer, err := casbin.NewEnforcer("config/keymatch_model", casbinGormAdapter)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	policyEnforcer.SetRoleManager(roleManager)
-
-	// Logs for casbin.
-	policyEnforcer.EnableLog(true)
-
-	// Load the policy from DB.
-	err = policyEnforcer.LoadPolicy()
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	// Save the policy back to DB.
-	err = policyEnforcer.SavePolicy()
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	return policyEnforcer, nil
-}
-
-func AddCreatePolicy(policyEnforcer *casbin.Enforcer, user string, url string) {
-	isAdded, err := policyEnforcer.AddPolicy(user, url, "POST")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Added policy create with result: %t", isAdded)
-}
-
-func AddUpdatePolicy(policyEnforcer *casbin.Enforcer, user string, url string) {
-	isAdded, err := policyEnforcer.AddPolicy(user, url, "POST")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Added policy update with result: %t", isAdded)
-}
-
-func AddDeletePolicy(policyEnforcer *casbin.Enforcer, user string, url string) {
-	isAdded, err := policyEnforcer.AddPolicy(user, url, "POST")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Added policy delete with result: %t", isAdded)
-}
-
-func AddGetPolicy(policyEnforcer *casbin.Enforcer, user string, url string) {
-	isAdded, err := policyEnforcer.AddPolicy(user, url, "GET")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Added policy get with result: %t", isAdded)
-
-	AddGetByIdPolicy(policyEnforcer, user, url + "/:id")
-}
-
-func AddGetByIdPolicy(policyEnforcer *casbin.Enforcer, user string, url string) {
-	isAdded, err := policyEnforcer.AddPolicy(user, url, "GET")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Added policy by id with result: %t", isAdded)
-}
--- go.mod
+++ go.mod
@@ -4,17 +4,16 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/casbin/casbin/v2 v2.2.2
 	github.com/casbin/gorm-adapter/v2 v2.0.3
-	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.1
-	github.com/jinzhu/gorm v1.9.10
+	github.com/jinzhu/gorm v1.9.11
 	github.com/jinzhu/now v1.1.1 // indirect
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/lib/pq v1.3.0 // indirect
+	github.com/lib/pq v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
-	github.com/spf13/viper v1.6.0
+	github.com/spf13/viper v1.6.3
 	github.com/stretchr/testify v1.5.0
 	github.com/swaggo/echo-swagger v1.0.0
 	github.com/swaggo/swag v1.6.5
