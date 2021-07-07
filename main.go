f0fb2730842e6ecf426aec820fa782b5d41017c3
s0j0hn <5699008-s0j0hn@users.noreply.gitlab.com>
2021-07-07 21:06:26 +0000

    Dev

--- main.go
+++ main.go
@@ -195,6 +195,15 @@ func main() {
 				//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
 				GetCertificate: autoTLSManager.GetCertificate,
 				NextProtos:     []string{acme.ALPNProto},
+				MinVersion:		tls.VersionTLS12,
+				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				PreferServerCipherSuites: true,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
 			},
 			//ReadTimeout: 30 * time.Second, // use custom timeouts
 		}
